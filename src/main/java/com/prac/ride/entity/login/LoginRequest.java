53718447bbfdc54aba140dbc58a4e0392a0204c4
Amar2107 <amrbl2107@gmail.com>
2025-04-05 15:06:53 +0530

    Login pojos added

---
+++ src/main/java/com/prac/ride/entity/login/LoginRequest.java
@@ -0,0 +1,22 @@
+package com.prac.ride.entity.login;
+
+
+public class LoginRequest {
+
+    private final String email;
+    private final String password;
+
+    public LoginRequest(String email, String password){
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getEmail(){
+        return this.email;
+    }
+
+
+    public String getPassword(){
+        return this.password;
+    }
+}
